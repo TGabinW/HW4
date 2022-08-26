7e3728ed17a850cbfb3982656bc0e224b5fe04ef
TGabinW <111113016+TGabinW@users.noreply.github.com>
2022-08-26 17:30:39 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿var set = new int[] { 15, 12, -6, 8, -9, 12, 0 };
+Console.WriteLine($"До сортировки");
+foreach (int n in set)
+    Console.Write(n + " ");
+int temp = 0;
+int perest = 0;
+for (int j = 0; j <= set.Length - 2; j++)
+{
+    for (int i = 0; i <= set.Length - 2; i++)
+    {
+        if (set[i] > set[i + 1])
+        {
+            perest++;
+
+            temp = set[i + 1];
+            set[i + 1] = set[i];
+            set[i] = temp;
+        }
+    }
+}
+
+Console.WriteLine();
+Console.WriteLine("Сортировка по возрастанию:");
+foreach (int n in set)
+    Console.Write(n + " ");
+Console.WriteLine();
+Console.Write("Операций по обмену элементов выполнено: " + perest);
+int per = 0;
+for (int j = 0; j <= set.Length - 2; j++)
+{
+    for (int i = 0; i <= set.Length - 2; i++)
+    {
+        if (set[i] < set[i + 1])
+        {
+            per++;
+
+            temp = set[i + 1];
+            set[i + 1] = set[i];
+            set[i] = temp;
+        }
+    }
+}
+Console.WriteLine();
+Console.WriteLine("Сортировка по убыванию:");
+foreach (int n in set)
+    Console.Write(n + " ");
+Console.WriteLine();
+Console.Write("Операций по обмену элементов выполнено: " + per);
